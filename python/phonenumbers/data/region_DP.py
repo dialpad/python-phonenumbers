03e3367b5efadb20db46306f4440160c4c0279ce
JerinDialpad <43014640+JerinDialpad@users.noreply.github.com>
2026-02-13 10:46:19 -0800

    update region_DP to include fixed_line, and fix sandbox location

--- python/phonenumbers/data/region_DP.py
+++ python/phonenumbers/data/region_DP.py
@@ -3,6 +3,7 @@ from ..phonemetadata import NumberFormat, PhoneNumberDesc, PhoneMetadata
 
 PHONE_METADATA_DP = PhoneMetadata(id='DP', country_code=803, international_prefix=None,
     general_desc=PhoneNumberDesc(national_number_pattern='\\d{10}', example_number='1000000141', possible_length=(10,)),
-    voip=PhoneNumberDesc(national_number_pattern='\\d{10}', example_number='1000000141', possible_length=(10,)),
+    fixed_line=PhoneNumberDesc(national_number_pattern='1\\d{9}', example_number='1000000141', possible_length=(10,)),
+    voip=PhoneNumberDesc(national_number_pattern='2\\d{9}', example_number='2000000141', possible_length=(10,)),
     number_format=[NumberFormat(pattern='(\\d{3})(\\d{7})', format='\\1\\2')],
     leading_zero_possible=False)
